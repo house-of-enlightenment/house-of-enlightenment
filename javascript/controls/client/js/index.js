6f58e5b1a8b4f9ef72fd226f6f72554dac78bd3f
jobevers <jobevers@users.noreply.github.com>
2017-07-15 17:27:47 -0400

    revert index.js back

--- javascript/controls/client/js/index.js
+++ javascript/controls/client/js/index.js
@@ -4,18 +4,8 @@ import ReactDom from "react-dom";
 import osc from "osc";
 import App from "./components/App.jsx";
 
-var oscPort = new osc.WebSocketPort({
-    url: "ws://localhost:3032", // URL to your Web Socket server.
-    metadata: true
-});
-oscPort.open();
 
-
-oscPort.on("ready", function () {
-  console.log('Websocket connected, rendering app');
-
-  ReactDom.render(
-    <App />,
-    document.querySelector(".js-app-mount")
-  );
-});
+ReactDom.render(
+  <App />,
+  document.querySelector(".js-app-mount")
+);
