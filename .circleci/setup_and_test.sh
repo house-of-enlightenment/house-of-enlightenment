4acef02f3baaf30136783b3255c25625f094ec2c
jobevers <jobevers@users.noreply.github.com>
2017-07-16 23:12:51 -0400

    do some linting on circleci

---
+++ .circleci/setup_and_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+virtualenv venv
+# the activate script has unbound variables so we need to stop
+# checking for those
+set +u
+source venv/bin/activate
+set -u
+
+# it is useful to see the trace so that when things fail we can see why
+set -o xtrace
+
+cd python
+pip install -r requirements.txt
+pip install pylint pep8
+pip install .
+
+pep8 hoe scripts
+pylint -E hoe scripts/*
