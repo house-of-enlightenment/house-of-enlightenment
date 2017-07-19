b040dd26718e60b05a27e163eecc015be1ce500f
Anthony Nicolaides <anthony.nicolaides@gmail.com>
2017-07-18 22:19:18 -0400

    creating some test animations

---
+++ python/effects/TonyTestAnimation.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('..')
+from hoe import color_utils
+from hoe.scene_manager import SceneDefinition
+from hoe.scene_manager import EffectDefinition
+from hoe.scene_manager import Effect
+from pydoc import locate
+
+class TonyEffect(Effect):
+    """Always return red"""
+    def next_frame(self, layout, time, n_pixels, osc_data):
+        return [(0,255,0) for ii, coord in enumerate(layout)] #TODO: faster with proper python array usage
+
+__all__= [
+    SceneDefinition("tony's scene", EffectDefinition("Tony's Effect", TonyEffect))
+ ]
