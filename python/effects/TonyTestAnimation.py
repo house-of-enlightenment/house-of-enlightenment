b284bc0c9603ca170127f3e31ddc8d9cc37f1326
David Foregger <foreggerd@gmail.com>
2017-07-23 15:30:46 -0400

    Added tony's effect to pixel branch prior to merge from master.

--- python/effects/TonyTestAnimation.py
+++ python/effects/TonyTestAnimation.py
@@ -8,9 +8,10 @@ from pydoc import locate
 
 class TonyEffect(Effect):
     """Always return red"""
-    def next_frame(self, layout, time, n_pixels, osc_data):
-        return [(0,255,0) for ii, coord in enumerate(layout)] #TODO: faster with proper python array usage
+    def next_frame(self, pixels, t, osc_data):
+        for ii, coord in enumerate(self.layout):
+            pixels[ii] = (0, 255, 0)
 
 __all__= [
-    #SceneDefinition("tony's scene", EffectDefinition("Tony's Effect", TonyEffect))
+    SceneDefinition("tony's scene", EffectDefinition("Tony's Effect", TonyEffect))
  ]
