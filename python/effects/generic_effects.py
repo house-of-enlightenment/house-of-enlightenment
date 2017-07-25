abba7a40370dc85ccb3215f1af63c348bda4970f
David Foregger <foreggerd@gmail.com>
2017-07-24 20:21:47 -0400

    Updated the adjustable effect to scale with 0-100 faders

--- python/effects/generic_effects.py
+++ python/effects/generic_effects.py
@@ -19,14 +19,19 @@ class SolidBackground(Effect):
 
 
 class AdjustableFillFromBottom(Effect):
+    def __init__(self, max_in=100, layout=None, n_pixels=None):
+        Effect.__init__(self, layout, n_pixels)
+        self.color_scale = 255.0 / max_in
+        self.row_scale = 216.0 / max_in
+
     def next_frame(self, pixels, t, collaboration_state, osc_data):
         for ii, coord in enumerate(self.layout.pixels):
             self.fill(pixels, t, coord, ii, osc_data)
 
     def fill(self, pixels, time, coord, ii, osc_data):
         # TODO Check for existance of fade
-        if not pixels[ii] and osc_data.stations[5].faders[0] > int(coord["row"]):
-            pixels[ii] = tuple([int(osc_data.stations[i].faders[0]) for i in range(3)])
+        if not pixels[ii] and osc_data.stations[5].faders[0] * self.row_scale > coord["row"]:
+            pixels[ii] = tuple([int(osc_data.stations[i].faders[0] * self.color_scale) for i in range(3)])
 
 
 __all__ = []
