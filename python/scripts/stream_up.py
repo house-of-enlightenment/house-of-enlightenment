902b2d42f21376165c95fb26f40ca98b3f490d62
jobevers <jobevers@users.noreply.github.com>
2017-07-27 23:32:27 -0400

    refactor Rainbow Flow effect

--- python/scripts/stream_up.py
+++ python/scripts/stream_up.py
@@ -3,6 +3,7 @@ flowing and twisting up the House of Enlightenment
 """
 
 from __future__ import division
+import argparse
 import json
 import math
 import Queue
@@ -27,6 +28,10 @@ YELLOW = (255, 255, 0)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--single', action='store_true')
+    args = parser.parse_args()
+
     # TODO: copy and paste is bad, mkay.
     client = opc.Client('localhost:7890')
     if not client.can_connect():
@@ -40,7 +45,11 @@ def main():
     server = osc_utils.create_osc_server()
     # pylint: disable=no-value-for-parameter
     server.addMsgHandler("/input", lambda *args: None)
-    effect = Effect(hoe_layout)
+    if args.single:
+        row = SingleColumn(hoe_layout)
+    else:
+        row = RainbowRow(hoe_layout)
+    effect = Effect(hoe_layout, row)
     render = Render(client, osc_queue, hoe_layout, [effect])
     render.run_forever()
 
@@ -80,15 +89,13 @@ class Render(object):
 
 
 class Effect(object):
-    def __init__(self, layout):
+    def __init__(self, layout, row):
         self.layout = layout
         self.pixels_per_frame = 3
-        self.rotation_speed = SpeedUpdate(3)
-        self.rotation = 0
+        self.row = row
 
     def start(self, now):
-        self.sv = SVTransition(now)
-        self.hue = HueTransition(now)
+        self.row.start(now)
         # need my own copy of pixels
         self.pixels = pixels.Pixels(self.layout)
 
@@ -98,14 +105,35 @@ class Effect(object):
         self.after_idx = np.concatenate((a, b), axis=1)
 
     def next_frame(self, now, pixels):
-        rainbow = self.get_rainbow(now)
-        self.rotate(rainbow, now)
-        # copy up and rotate
-        self.pixels[self.after_idx] = self.pixels[self.before_idx]
-        # grab the 10th row and copy up
+        rainbow = self.row(now)
+        # copy to the first few rows
         for i in range(self.pixels_per_frame):
-            self.pixels[10 + i, :] = self.pixels[10, :]
+            self.pixels[10 + i, :] = rainbow
+        # copy everything else up and rotate
+        self.pixels[self.after_idx] = self.pixels[self.before_idx]
         pixels[:] = self.pixels[:]
+
+
+class RainbowRow(object):
+    """Creates a row that is an arc in the HSV color space.
+
+    Each update gradually shifts the original arc towards another
+    arc. Once that is hit, another random arc is choosen and we
+    continue the process.  Additionally the row is rotated.
+
+    The speed of the transitions and rotations vary randomly.
+    """
+    def __init__(self, layout):
+        self.layout = layout
+        self.rotate = Rotate(self.layout.columns)
+
+    def start(self, now):
+        self.sv = SVTransition(now)
+        self.hue = HueTransition(now)
+
+    def __call__(self, now):
+        rainbow = self.get_rainbow(now)
+        return self.rotate(rainbow, now)
 
     def get_rainbow(self, now):
         hue = self.hue.update(now)
@@ -115,26 +143,51 @@ class Effect(object):
             (color_utils.rainbow(self.layout.columns / 2, hue[0], hue[1], sat, val),
              color_utils.rainbow(self.layout.columns / 2, hue[1], hue[0], sat, val)))
 
-    def rotate(self, rainbow, now):
+
+class SingleColumn(object):
+    """Creates a row that is just one red pixel.
+
+    Can be useful to see how the shape flows up the HoE.
+    """
+    def __init__(self, layout):
+        self.layout = layout
+        self.rotate = Rotate(layout.columns)
+
+    def start(self, now):
+        pass
+
+    def __call__(self, now):
+        row = np.zeros((self.layout.columns, 3), np.uint8)
+        row[10, :] = (255, 0, 0)
+        return self.rotate(row, now)
+
+
+class Rotate(object):
+    def __init__(self, n):
+        self.rotation_speed = SpeedUpdate(3)
+        self.rotation = 0
+        self.n = n
+
+    def __call__(self, arr, now):
         # vary the rotation speed
         if np.random.random() < self.rotation_speed.update(now):
-            self.rotation = (self.rotation + 1) % self.layout.columns
+            self.rotation = (self.rotation + 1) % self.n
         if self.rotation == 0:
-            self.pixels[10, :] = rainbow
+            return arr
         else:
-            self.pixels[10, :-self.rotation] = rainbow[self.rotation:]
-            self.pixels[10, -self.rotation:] = rainbow[:self.rotation]
+            # using negative rotation here seems to be less impressive
+            # I think its because it matches with the rotation of
+            # the entire structure.
+            return rotate(arr, self.rotation)
 
-    def __setitem__(self, key, value):
-        idx = self.layout.grid[key]
-        self.pixels[idx] = value
 
-    def __getitem__(self, key):
-        idx = self.layout.grid[key]
-        return self.pixels[idx]
+# idx can be negative
+def rotate(arr, idx):
+    return np.concatenate((arr[idx:], arr[:idx]))
 
 
 class SpeedUpdate(object):
+    """Every `delay` seconds, pick a new random value"""
     def __init__(self, delay):
         self.next_update = 0
         self.delay = delay
@@ -147,16 +200,14 @@ class SpeedUpdate(object):
         return self.value
 
 
-class SVTransition(object):
+class Transition(object):
     def __init__(self, now):
-        self.end = np.array([np.random.randint(128, 256), np.random.randint(196, 256)])
+        self.end = self.rnd_pt()
         self._reset(now)
 
     def _reset(self, now):
         self.start = self.end
-        # pick a saturation between 128-256 (128 is very pastel, 256 is full saturation)
-        # pick a value between 192-256 (probably a very minor effect)
-        self.end = np.array([np.random.randint(128, 256), np.random.randint(196, 256)])
+        self.end = self.rnd_pt()
         self.length = np.random.rand() * 3 + 1
         self.start_time = now
 
@@ -168,23 +219,21 @@ class SVTransition(object):
         return self.start + delta
 
 
-class HueTransition(object):
-    def __init__(self, now):
-        self.end = self.rnd_pt()
-        self._reset(now)
+class SVTransition(Transition):
+    """Transition from one plane in Saturation-Value space to another"""
+    def rnd_pt(self):
+        # pick a saturation between 128-256 (128 is very pastel, 256 is full saturation)
+        # pick a value between 192-256 (probably a very minor effect)
+        return np.array([np.random.randint(128, 256), np.random.randint(196, 256)])
 
-    def _reset(self, now):
-        self.start = self.end
-        self.end = self.rnd_pt()
-        self.length = np.random.rand() * 3 + 1
-        self.start_time = now
 
+class HueTransition(Transition):
     def rnd_pt(self):
         # pick a hue to start with
         start = np.random.randint(0, 256)
         # pick how much of the color wheel we're going to take
         # a longer slice will have more colors
-        length = np.random.randint(32, 96)
+        length = np.random.randint(32, 256)
         end = start + length
         return np.array([start, end])
 
@@ -194,11 +243,7 @@ class HueTransition(object):
         # This is always going clockwise
         #
         # TODO: on reset, pick a direction
-        elapsed = now - self.start_time
-        if elapsed > self.length:
-            self._reset(now)
-        delta = (self.end - self.start) * (now - self.start_time) / self.length
-        return self.start + delta
+        return super(HueTransition, self).update(now)
 
 
 if __name__ == '__main__':
