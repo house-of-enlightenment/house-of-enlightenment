5ae2a4c584b40cf84b08c8e35d1904f180c8c820
David Foregger <foreggerd@gmail.com>
2017-07-29 19:06:44 -0400

    Added a 'rising tide' sample

--- python/effects/generic_effects.py
+++ python/effects/generic_effects.py
@@ -2,6 +2,8 @@ from hoe import color_utils
 from hoe.animation_framework import Scene
 from hoe.animation_framework import Effect
 from hoe.animation_framework import CollaborationManager
+from hoe.animation_framework import MultiEffect
+from random import getrandbits
 from hoe.layout import layout
 
 
@@ -34,4 +36,79 @@ class AdjustableFillFromBottom(Effect):
                 [int(osc_data.stations[i].faders[0] * self.color_scale) for i in range(3)])
 
 
-__all__ = []
+class RisingTide(Effect):
+    def __init__(self,
+                 target_color=(255, 255, 255),
+                 start_color=(10, 10, 10),
+                 start_column=0,
+                 end_column=None,
+                 bottom_row=2,
+                 top_row=None):
+        self.target_color = target_color
+        self.start_color = start_color
+        self.start_column = start_column
+        self.end_column = end_column
+        self.bottom_row = bottom_row
+        self.top_row = top_row
+        self.curr_top = self.bottom_row
+        self.curr_bottom = self.bottom_row
+        self.completed = False
+
+        self.colors = [start_color]
+        self.color_inc = tuple(
+            map(lambda t, s: (0.0 + t - s) / (self.top_row - self.bottom_row), target_color,
+                start_color))
+        print self.start_color, self.target_color, self.color_inc
+
+    def next_frame(self, pixels, t, collaboration_state, osc_data):
+        if self.curr_top < self.top_row:
+            for i, r in enumerate(range(self.curr_bottom, self.curr_top)):
+                pixels[r, self.start_column:self.end_column] = self.colors[i]
+
+            self.colors = [tuple(map(lambda c, i: c + i, self.colors[0], self.color_inc))
+                           ] + self.colors
+            self.curr_top += 1
+        elif self.curr_bottom < self.top_row:
+            self.colors = self.colors[:-1]
+            self.curr_bottom += 1
+            for i, r in enumerate(range(self.curr_bottom, self.curr_top)):
+                pixels[r, self.start_column:self.end_column] = self.colors[i]
+        else:
+            self.completed = True
+
+    def is_completed(self, t, osc_data):
+        return self.completed
+
+
+class TideLauncher(MultiEffect):
+    def before_rendering(self, pixels, t, collaboration_state, osc_data):
+        MultiEffect.before_rendering(self, pixels, t, collaboration_state, osc_data)
+        for s in range(layout().sections):
+            if osc_data.stations[s].buttons:
+                self.launch_effect(t, s)
+
+    def launch_effect(self, t, s):
+        per_section = int(layout().columns / layout().sections)
+        c = (bool(getrandbits(1)), bool(getrandbits(1)), bool(getrandbits(1)))
+        print c
+        if not any(c):  #  Deal with all 0's case
+            c = (True, True, True)
+        print c
+        start_color = (c[0] * 10, c[1] * 10, c[2] * 10)
+        target_color = (c[0] * 255, c[1] * 255, c[2] * 255)
+        e = RisingTide(
+            start_column=s * per_section,
+            end_column=(s + 1) * per_section,
+            bottom_row=0,
+            top_row=216,
+            target_color=target_color,
+            start_color=start_color)
+        self.effects.append(e)
+
+
+class NoOpCollaborationManager(CollaborationManager):
+    def compute_state(self, t, collaboration_state, osc_data):
+        pass
+
+
+__all__ = [Scene("risingtide", NoOpCollaborationManager(), SolidBackground(), TideLauncher())]
