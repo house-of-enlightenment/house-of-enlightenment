6c83a46178e77c77820bb391c22fbf342a61834e
Mike Lambert <lax4mike@gmail.com>
2017-08-01 12:30:11 -0400

    Adding button colors

--- javascript/controls/client/js/components/Button/Button.jsx
+++ javascript/controls/client/js/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { func, oneOf } from "prop-types";
 
 
@@ -10,13 +11,40 @@ const propTypes = {
 
 const Button = ({ id, onClick }) => {
 
+  const buttonClasses = classNames(getButtonColorClass(id), {
+    "is-disabled": Math.random() > 0.5
+  });
+
+
+
   return (
-    <button onClick={onClick}>
-      { id }
+    <button onClick={onClick} className={buttonClasses}>
+      {/* { id } */}
     </button>
   );
 
 };
+
+
+function getButtonColorClass(id){
+
+  console.log("ID", id);
+
+  switch (id){
+    case "0":
+      return "button--red";
+    case "1":
+      return "button--green";
+    case "2":
+      return "button--blue";
+    case "3":
+      return "button--yellow";
+    default:
+      return "button--white";
+
+  }
+
+}
 
 Button.propTypes = propTypes;
 
