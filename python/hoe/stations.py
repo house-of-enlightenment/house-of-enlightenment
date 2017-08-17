df744c5a33a0236375d8faa6f0363ce17360553f
David Foregger <foreggerd@gmail.com>
2017-08-17 01:03:46 -0400

    Added client connection support. Disconnection not complete yet

--- python/hoe/stations.py
+++ python/hoe/stations.py
@@ -3,29 +3,6 @@ import socket
 
 from hoe.osc_utils import update_buttons
 
-class StationClient(object):
-    def __init__(self, s_id, host, port):
-        self.s_id = s_id  # Not currently in use but could be useful in the future
-        self.host = host
-        self.port = port
-        self.socket = None
-
-    def _ensure_connected(self):
-        if self.socket:
-            return
-
-        # TODO - Error handling!
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
-
-    def send(self, buttons):
-        self._ensure_connected()
-        data = reduce(lambda x,y: str(x)+str(y), buttons)
-        print "Sending data %s to %s" % (data, self.socket)
-        self.socket.send(data)
-
-        # TODO handle connection/disconnect
-
 
 class StationButtons(object):
     BUTTON_OFF = 0
@@ -59,6 +36,68 @@ class StationButtons(object):
             self._remote_client.send(self._buttons)
             # update_buttons(self._remote_client, self.station_id, self._buttons)
             # TODO: MAJOR - Send buttons to arduino controllers
+
+
+class StationClient(object):
+    def __init__(self, s_id, host, port, timeout=0.5):
+        self.s_id = s_id  # Not currently in use but could be useful in the future
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self._socket = None
+
+    def _ensure_connected(self):
+        """Set up a connection if one doesn't already exist.
+
+        Return True on success or False on failure.
+
+        """
+        if self._socket:
+            self._debug('_ensure_connected: already connected, doing nothing')
+            return True
+
+        try:
+            self._debug('_ensure_connected: trying to connect to {}:{}', self.host, self.port)
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.settimeout(self.timeout)
+            self._socket.connect((self.host, self.port))
+            self._debug('_ensure_connected:    ...success')
+            return True
+        except socket.error:
+            self._debug('_ensure_connected:    ...failure')
+            self._socket = None
+            return False
+
+    def disconnect(self):
+        """Drop the connection to the server, if there is one."""
+        self._debug('disconnecting')
+        if self._socket:
+            self._socket.close()
+        self._socket = None
+
+    def _debug(self, msg, *args):
+        # TODO  Move verbose to state and IF on that, or something similar
+        print msg.format(*args)
+
+    def send(self, buttons):
+        is_connected = self._ensure_connected()
+        if not is_connected:
+            self._debug('send: not connected.  ignoring these pixels.')
+            return False
+
+        data = reduce(lambda x, y: str(x) + str(y), buttons)
+        self._debug("Sending data %s to %s" % (data, self._socket))
+        try:
+            self._socket.send(data)
+        except socket.error:
+            self._debug('send: connection lost.  could not send button data.')
+            self._socket = None
+            return False
+
+        return True
+
+
+        # TODO handle connection/disconnect
 
 
 class OscStationClient(object):
