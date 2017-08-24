0ef975f2a0e00209a3be1ba6106535f2dc4e6bb6
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2017-08-24 11:02:42 -0400

    make test loop twice

--- python/tests/run_all_scenes_with_reader.py
+++ python/tests/run_all_scenes_with_reader.py
@@ -8,7 +8,7 @@ import subprocess
 
 p1 = subprocess.Popen(['python', 'socket_reader.py'])
 try:
-    subprocess.check_call(['python', 'run_all_scenes.py'])
+    subprocess.check_call(['python', 'run_all_scenes.py --loops=2'])
 finally:
     p1.terminate()
     p1.wait
